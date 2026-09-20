2136aaf136b7e4ddf0942ef36a7f89ffcf5cb436
Jazil Jafar <jazilhani@gmail.com>
2026-09-20 11:03:07 +0530

    kanban finished doing last minute works

--- KanbanAPI.js
+++ KanbanAPI.js
@@ -1,97 +1,83 @@
 export default class KanbanAPI {
-    static getItems(columnId){
+    static getItems(columnId) {
         const column = read().find(column => column.id == columnId);
-
-        if(!column){
+        if (!column) {
             return [];
-        };
+}
         return column.items;
-    };
+    }
 
-    static insertItem(columnId, content){
+    static insertItem(columnId, content) {
         const data = read();
         const column = data.find(column => column.id == columnId);
-        const item = {
-            id: Math.floor(Math.random() * 100000),
-            content
-        };
-
         if (!column) {
-            throw new Error("coulumn not finded");
+            throw new Error("Column not found.");
+        }
+        const item = {
+            id: Date.now(),
+            content
         };
         column.items.push(item);
         save(data);
 
         return item;
-    };
-
-    static updateItem(itemId, newProps){
+}
+    static updateItem(itemId, newProps) {
         const data = read();
         const [item, currentColumn] = (() => {
-          for (const column of data) {
-            const item = column.items.find(item => item.id == itemId);
-
-            if (item) {
-                return [item, column];
-            };
-          };
+            for (const column of data) {
+                const item = column.items.find(item => item.id == itemId);
+                if (item) {
+                    return [item, column];
+                }
+            }
+            return [null, null]; 
         })();
-        if(!item){
-            throw new Error("item not found");
+
+        if (!item) {
+            throw new Error("Item not found.");
         }
 
         item.content = newProps.content === undefined ? item.content : newProps.content;
 
         if (
-            newProps.columnId !== undefined
-            && newProps.position !== undefined
-        ){
-            const targetColumn = data.find(column => column.id == newProps.columnId);
+            newProps.columnId !== undefined &&
+            newProps.position !== undefined
+        ) {
+    const targetColumn = data.find(column => column.id == newProps.columnId);
 
-            if(!targetColumn){
-                throw new Error("Target Column not found.");
+            if (!targetColumn) {
+                throw new Error("Target column not found.");
             }
-
             currentColumn.items.splice(currentColumn.items.indexOf(item), 1);
-        
-            targetColumn.item.splice(newProps.position, 0, item);
-        };
-        save(data);
-    };
-    static deleteItem(itemId){
-       const data = read();
-
-       for (const column of data){
-        const item = column.items.find(item => item.id == itemId);
-
-        if(item){
-            column.items.splice(column.items.indexOf(item), 1);
-        }
-       }
-       save(data);
+            targetColumn.items.splice(newProps.position, 0, item);
     }
-};
-function read(){
-    const json = localStorage.getItem("kanban-data");
 
-    if (!json){
-        return[
-            {
-             id:1,
-             items:[]
-            },
-            {
-                id:2,
-                items: []
-            },
-            {
-                id:3,
-                items:[]
-            }
-    ];
-    };
+        save(data);
+    }
+    static deleteItem(itemId) {
+        const data = read();
+        for (const column of data) {
+            const item = column.items.find(item => item.id == itemId);
+            if (item) {
+                column.items.splice(column.items.indexOf(item), 1);
+                break;
+}
+        }
+
+        save(data);}
+}
+function read() {
+    const json = localStorage.getItem("kanban-data");
+    if (!json) {
+        return [
+            { id: 1, items: [] },
+            { id: 2, items: [] },
+            { id: 3, items: [] }
+        ];
+    }
     return JSON.parse(json);
-};
-function save(data){
+}
+function save(data) {
     localStorage.setItem("kanban-data", JSON.stringify(data));
-};
+}

211a589c1ed441746b84be490f3715420c24b690
Jazil Jafar <jazilhani@gmail.com>
2026-09-23 16:52:37 +0530

    working on layout

---
+++ my/hot/kanban.js
@@ -0,0 +1,21 @@
+export default class kanban {
+    constructor(root){
+      this.root = root;
+    }
+    static columns() {
+        return [
+            {
+                id: 1,
+                title: "Spark 🧨"
+            },
+            {
+                id: 2,
+                title: "On Fire 🔥"
+            },
+            {
+                id: 3,
+                title: "Ash ⚱️"
+            }
+        ];
+    };
+};

1d5295a53f727a4a15738f12bed6e5ad50d60373
Jazil Jafar <jazilhani@gmail.com>
2026-09-24 10:38:07 +0530

    deleting items added:

--- my/hot/item.js
+++ my/hot/item.js
@@ -1,3 +1,4 @@
+import KanbanAPI from "./KanbanAPI.js";
 export default class item {
     constructor(id, content) {
       this.elements = {};
@@ -5,13 +6,28 @@ export default class item {
       this.elements.input = this.elements.root.querySelector(".kanban__item-input");
 
       this.elements.root.dataset.id = id;
-      this.elements.input = content;
+      this.elements.input.textContent = content;
       this.content = content;
       
       const onedit = () => {
         const newcontent = this.elements.input.textContent.trim();
+        if(newcontent == this.content){
+          return;
+        }
+        this.content = newcontent;
+        KanbanAPI.updateItem(id, {
+          content: this.content
+        });
       };
-      this.elements.input.add
+      this.elements.input.addEventListener("blur", onedit);
+      this.elements.root.addEventListener("dblclick", () => {
+         const check = confirm("are you shure to delete this hot thing?");
+         if(check){
+          KanbanAPI.deleteitem(id);
+          this.elements.input.removeEventListener("blur", onedit);
+          this.elements.root.parentElement.removeChild(this.elements.root);
+         };
+      });
     };
     static createroot(){
        const range = document.createRange();
@@ -21,5 +37,5 @@ export default class item {
          <div class="kanban__item-input" contenteditable></div>
          </div>
         `).children[0];
-    }
+    };
 };
